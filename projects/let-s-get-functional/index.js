ff7e403c96b89fa3bbc69df7b28efb2ed2a5fb0a
Darius Cooper <kudoreha9@gmail.com>
2024-01-15 20:20:46 +0000

    describe the work you have completed

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -22,24 +22,144 @@ var _ = require(/* Replace this with the name of your lodown! */);
  */
 
 var maleCount = function(array) {
-
+      // FIND THE NUMBER OF MALE CUSTOMERS
+     // USING FILTER METHOD
+     var a = array.filter(function (obj) {
+      // NOW LOOPING THROUGH ARRAY
+       // RETURN OBJ MALE PROPERTY IF TRUE
+       return obj.gender === 'male';
+    });
+//console.log(a);
+// USING LEGNTH PROPERTY TO COUNT THE NUMBER OF MALES IN OBJECT
+ var count = a.length
+ //console.log(count);
+ return count;
 };
 
-var femaleCount;
+var femaleCount = function(array) {
+  // FIND THE NUMBER OF FEMALE CUSTOMERS
+   // USING REDUCE METHOD
+    var a = array.reduce(function(acc, curr) {
+      // NOW LOOPING THROUGH ARRAY
+       // IF CURRENT OBJECT HAS A FEMALE KEY VALUE
+        if (curr["gender"] === "female") {
+          // ADD 1 TO ACC
+           acc += 1;
+        }
+        return acc;
+    }, 0)
+    // RETURN NUMBER
+    return a;
+};
 
-var oldestCustomer;
+var oldestCustomer = function(array) {
+   // FIND THE OLDEST CUSTOMERS NAME
+   // USING REDUCE METHOD
+   var a = array.reduce(function(acc, curr) {
+    // NOW LOOPING THROUGH ARRAY
+     // IF CURRENT AGE IS GREATER THAN AGE IN FIRST ELEMENT
+    if (curr.age > array[0].age) {
+        // ASSIGN ACCUMULATOR TO CURRENT OBJECT NAME
+         acc = curr.name;
+      }
+    // RETURN ACCUMULATOR
+    return acc;
+  }, '');
 
-var youngestCustomer;
+return a;
+};
 
-var averageBalance;
+var youngestCustomer = function(array) {
+   // FIND THE YOUNGEST CUSTOMERS NAME
+   // USING REDUCE METHOD
+   var a = array.reduce(function(acc, curr) {
+    // NOW LOOPING THROUGH ARRAY
+     // IF CURRENT AGE IS LESS THAN THAN AGE IN FIRST ELEMENT
+    if (curr.age <= 22) {
+        // ASSIGN ACCUMULATOR TO CURRENT OBJECT NAME
+         acc = curr.name;
+      }
+    // RETURN ACCUMULATOR
+    return acc;
+  }, '');
 
-var firstLetterCount;
+return a;
+};
 
-var friendFirstLetterCount;
+var averageBalance = function(array) {
+  // FIND THE AVERAGE BALANCE OF ALL CUSTOMERS
+   // USING MAP METHOD
+   var a = array.map(function(money) {
+    // NOW LOOPING THROUGH THE ARRAY
+    // USING .replace() METHOD
+     var b = money.balance.replace(/,/g, '');
+    return b.replace(/\$/g, '');
+  })
+ //console.log(a);
+ // USING REDUCE METHOD
+  var sum = a.reduce(function(acc, curr) {
+    // NOW LOOPING THROUGH ARRAY
+     // USING NUMBER METHOD TO TURN STRINGS INTO NUMBERS
+     return acc += Number(curr);
+  }, 0)
+// RETURNING THE SUM DIVDED BY 8
+ return sum/8; 
+}
 
-var friendsCount;
+var firstLetterCount = function(array, letter) {
+  // FIND HOW MANY CUSTOMERS NAMES BEGINS WITH A GIVEN LETTER
+    // USING FILTER METHOD 
+    var a = array.filter(function(person) {
+      // NOW LOOPING THROUGH ARRAY
+       return person["name"].charAt() === letter
+    })
+  // RETURNING NUMBER OF NAMES
+  return a.length;
+};
 
-var topThreeTags;
+var friendFirstLetterCount = function(array, customer, letter) {
+  // HOW MANY FRIENDS OF A GIVEN CUSTOMER HAVE NAMES THAT START WITH A GIVEN LETTER
+    // USING FILTER METHOD
+   //console.log(array[0].friends)
+   var a = array.filter(function(person) {
+    // NOW LOOPING THROUGH ARRAY
+     // IF OBJECT NAME PROPERTY IS THE CUSTOMER
+      return person["name"] === customer;
+  })
+//console.log(a);
+// USING FILTER METHOD AGAIN
+ var friendsArray = a[0]["friends"].filter(function(string) {
+   // NOW LOOPING THROUGH ARRAY AGAIN
+    // IF OBJECT NAME PROPERTY VALUE HAS A STRING THAT STARTS WITH LETTER
+     return string["name"].charAt() === letter;
+ })
+// RETURNING LENGTH OF THE ARRAY
+ return friendsArray.length;
+};
+
+var friendsCount = function(array, name) {
+   // FIND THE CUSTOMERS NAMES THAT HAVE GIVEN A CUSTOMERS NAME IN THEIR FRIENDS LIST
+  var output = [];
+  // USING FOR LOOP
+    for (var i = 0; i <= array.length - 1; i++) {
+        // USING FOR LOOP AGAIN
+         for (var j = 0; j <= array[i]["friends"].length - 1; j++) {
+           // IF NAME PROPERTY IN OBJECT MATCHES NAME
+            if (array[i]["friends"][j]["name"] === name) {
+              // PUSH ARRAY ELEMENT NAME INTO OUTPUT
+               output.push(array[i]["name"]);
+            }  
+         }
+    }
+ //console.log(a);
+ // RETURNING ARRAY
+  return output;
+};
+
+var topThreeTags = function(array) {
+  // FIND THE THREE MOST COMMON TAGS AMONG ALL CUSTOMERS; ASSOCIATED TAGS
+   
+};
 
 var genderCount;
 
